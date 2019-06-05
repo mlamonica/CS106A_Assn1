10716f1885fb425097481dbf994495c31af1258e
CS106A <>
2019-06-05 13:24:03 -0500

    {"types":"C","runs":0}

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -24,7 +24,10 @@ public class StoneMasonKarel extends SuperKarel {
 				move();
 				verifyBeeperPresence();
 			}
-			
+			turnAround();
+			moveToWall();
+			turnLeft();
+			moveALot(4);
 		}
 		
 	}
